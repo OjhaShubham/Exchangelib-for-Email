2ea617a98a070a4f82e531015d7e7a3084c359ef
Shubham Ojha <35261062+OjhaShubham@users.noreply.github.com>
2022-02-21 11:55:29 +0530

    Update trail_exchange_lib.py

--- trail_exchange_lib.py
+++ trail_exchange_lib.py
@@ -2,6 +2,5 @@ from exchangelib import Credentials, Account
 
 credentials = Credentials('shubhamojha@gmail.com', 'ShuuDonotTry')
 account = Account('shubhamojha@gmail.com', credentials=credentials, autodiscover=True)
-# print("before loop")
 for item in account.inbox.all().order_by('-datetime_received')[:10]:)
     print(item.subject, item.sender, item.datetime_received)
